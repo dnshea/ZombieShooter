19f3c7b766798516c9157691cce29671e11ba2f7
nmmiranda20 <166544622+nmmiranda20@users.noreply.github.com>
2024-05-02 11:49:18 -0700

    stuff

---
+++ Assets/_Scripts/UIManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+/*
+ * Nilo Miranda
+ * 5/2/24
+ * Focuses on UI manager and everything related towards it.
+ */
+/*
+public class UIManager : MonoBehaviour
+{
+
+    public PlayerController PlayerController;
+    public TMP_Text Lives;
+   public TMP_Text Score;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Lives.text = "HP: " + PlayerController.HP;
+        Score.text = "Score: " + PlayerController.Score;
+    }
+}
+*/
