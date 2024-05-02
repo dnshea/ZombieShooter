ccbc0aad5176228aa6adb4fb5ddddb21ef64322f
Splashhy <realsplashysplash@gmail.com>
2024-05-02 10:45:44 -0700

    zombie

--- Assets/_Scripts/Zombie.cs
+++ Assets/_Scripts/Zombie.cs
@@ -6,35 +6,17 @@ public class Zombie : MonoBehaviour
 {
     public int speed;
     public GameObject player;
+    Rigidbody rb;
+    private bool seePlayer;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     void Update()
     {
-        Vector3 localPosition = player.transform.position - transform.position;
-        localPosition = localPosition.normalized;
-        transform.Translate(localPosition.x * Time.deltaTime * speed, localPosition.y * Time.deltaTime * speed, localPosition.z * Time.deltaTime * speed);
-        if (HitWall() == true)
-        {
-
-        }
+        if (seePlayer)
+        transform.LookAt(player.transform);
+        rb.AddForce(speed * Time.deltaTime * transform.forward);
     }
-    public bool HitWall()
-    {
-        bool hitWall = false;
-        Vector3 raycastOrgin = transform.position;
-        Vector3 originOffset = new Vector3(0, 0.9f, 0);
-        float playerWidth = 0.5f;
-        RaycastHit hit;
-        if (Physics.Raycast(raycastOrgin, Vector3.forward, out hit, playerWidth))
-        {
-            hitWall = true;
-        }
-        if (Physics.Raycast(raycastOrgin + originOffset, Vector3.forward, out hit, playerWidth))
-        {
-            hitWall = true;
-        }
-        if (Physics.Raycast(raycastOrgin - originOffset, Vector3.forward, out hit, playerWidth))
-        {
-            hitWall = true;
-        }
-        return hitWall;
-    }
+   
 }
