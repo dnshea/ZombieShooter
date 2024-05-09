954c4407fcd61054137207f9848f12265e5c5e18
Splashhy <realsplashysplash@gmail.com>
2024-05-09 11:46:52 -0700

    rigid

--- Assets/_Scripts/Spawner.cs
+++ Assets/_Scripts/Spawner.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
@@ -11,44 +12,39 @@ public class Spawner : MonoBehaviour
     public GameObject Bomber;
     public GameObject Boss;
     public GameObject Health;
-
+    //Level One Spawn Points
     public Transform spawnOne;
     public Transform spawnTwo;
     public Transform spawnThree;
     public Transform spawnFour;
     public Transform spawnFive;
-    public Transform spawnAir1;
-    public Transform spawnAir2;
-    public Transform spawnAir3;
-    public Transform spawnAir4;
-    public Transform spawnAir5;
+    public Transform spawnAir11;
+    public Transform spawnAir12;
+    public Transform spawnAir13;
+    public Transform spawnAir14;
+    public Transform spawnAir15;
+    //Level Two Spawn Points
+    public Transform spawnOne2;
+    public Transform spawnTwo2;
+    public Transform spawnThree2;
+    public Transform spawnFour2;
+    public Transform spawnFive2;
+    public Transform spawnAir21;
+    public Transform spawnAir22;
+    public Transform spawnAir23;
+    public Transform spawnAir24;
+    public Transform spawnAir25;
 
     public int enemyCount;
 
+    private int waveNum = 1;
     // Start is called before the first frame update
-    void Start()
-    {
-     if (levelOne)
-        {
-            WaveOne();
-        }
-     else
-        {
-            WaveFour();
-        }
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        WaveOne();
     }
-    private void SpawnEnemy(GameObject enemy)
-    {
-        Instantiate(enemy, transform.position, transform.rotation);
-        enemyCount++;
-    }
-    private void SpawnEnemyAir(GameObject enemy)
+    private void SpawnEnemy(GameObject enemy, Transform spawnPoint)
     {
         Instantiate(enemy, transform.position, transform.rotation);
         enemyCount++;
@@ -56,19 +52,62 @@ public class Spawner : MonoBehaviour
 
     private void WaveOne()
     {
-        SpawnEnemy(Zombie);
+        if(waveNum == 1)
+        {
+            SpawnEnemy(Zombie, spawnOne);
+            spawnDelay(3);
+
+            waveNum++;
+        }
+        else if(waveNum == 2 && enemyCount == 0)
+        {
+            WaveTwo();
+        }
+
     }
     private void WaveTwo()
     {
-
+        if (waveNum == 2)
+        {
+            SpawnEnemy(Zombie, spawnOne);
+            waveNum++;
+        }
+        else if (waveNum == 3 && enemyCount == 0)
+        {
+            WaveThree();
+        }
     }
     private void WaveThree()
     {
-
+        if (waveNum == 3)
+        {
+            SpawnEnemy(Zombie, spawnOne);
+            waveNum++;
+        }
+        else if (waveNum == 4 && enemyCount == 0)
+        {
+            WaveFour();
+        }
     }
     private void WaveFour()
     {
+        if (waveNum == 1)
+        {
+            SpawnEnemy(Zombie, spawnOne);
+            waveNum++;
+        }
+        else if (waveNum == 5 && enemyCount == 0)
+        {
+            WaveFive();
+        }
+    }
+    private void WaveFive()
+    {
 
+    }
+    private IEnumerator spawnDelay(float time)
+    {
+        yield return new WaitForSeconds(time);
     }
 
 }
