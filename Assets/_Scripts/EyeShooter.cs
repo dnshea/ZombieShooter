91bc141ec32a720810cb1012fe2beb2e077054a1
dnshea <dcoven10@gmail.com>
2024-05-10 21:20:45 -0700

    Boss Changes

--- Assets/_Scripts/EyeShooter.cs
+++ Assets/_Scripts/EyeShooter.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class EyeShooter : MonoBehaviour
 {
+    public GameObject laserballPrefab;
+    public float spawnRate;
+    public int hp;
+    public GameObject player;
+    public Transform eye;
+    public GameObject boss;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        InvokeRepeating("SpawnLaserBall", 1, spawnRate);
 
-    // Update is called once per frame
-    void Update()
+    }
+    private void SpawnLaserBall()
     {
-        
+        GameObject newLaser = Instantiate(laserballPrefab, eye.transform.position, eye.transform.rotation);
+    }
+    private void Update()
+    {
+        if (hp <= 0)
+        {
+            player.GetComponent<PlayerController>().score += 100;
+            boss.GetComponent<Boss>().eyes--;
+            Destroy(this.gameObject);
+        }
     }
 }
